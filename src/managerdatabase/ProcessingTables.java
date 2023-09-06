f079294f4a2338d46538df1835cb93ce9a4ff733
Georgvit <ifreyd@mail.ru>
2023-09-06 23:42:17 +0300

    ProcessingTables_v1

---
+++ src/managerdatabase/ProcessingTables.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managerdatabase;
+
+/**
+ *
+ * @author Егор
+ */
+public interface ProcessingTables {
+   void addData();
+   void delData(String id);
+   void updateData();
+    
+}
