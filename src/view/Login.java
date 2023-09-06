c93960a913567a7cdb2f06ef8f4d232584487878
Georgvit <ifreyd@mail.ru>
2023-09-06 23:35:29 +0300

    login_valid

--- src/view/Login.java
+++ src/view/Login.java
@@ -102,6 +102,11 @@ public class Login extends javax.swing.JFrame {
         userTxt.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
 
         passwordTxt.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        passwordTxt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                passwordTxtActionPerformed(evt);
+            }
+        });
 
         jLabel2.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(0, 0, 0));
@@ -200,6 +205,34 @@ public class Login extends javax.swing.JFrame {
 
     private void loginBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_loginBtnMouseClicked
         // TODO add your handling code here:
+        entrance();
+    }//GEN-LAST:event_loginBtnMouseClicked
+
+    private void clearBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_clearBtnMouseClicked
+        // TODO add your handling code here:
+        userTxt.setText("");
+        passwordTxt.setText("");
+    }//GEN-LAST:event_clearBtnMouseClicked
+
+    private void passwordTxtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordTxtActionPerformed
+        // TODO add your handling code here:
+        entrance();
+    }//GEN-LAST:event_passwordTxtActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void startLogin() {
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+
+    //Проверка пользователя на валидность и открытие по типу
+    public void entrance() {
         ConnectDb con = new ConnectDb();
         String userType = con.validUser(userTxt.getText(), passwordTxt.getText());
         if (con.isHasNext()) {
@@ -225,47 +258,6 @@ public class Login extends javax.swing.JFrame {
         } else {
             JOptionPane.showMessageDialog(this, "WRONG USER ID OR PASSWORD");
         }
-    }//GEN-LAST:event_loginBtnMouseClicked
-
-    private void clearBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_clearBtnMouseClicked
-        // TODO add your handling code here:
-        userTxt.setText("");
-        passwordTxt.setText("");
-    }//GEN-LAST:event_clearBtnMouseClicked
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void startLogin() {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Login().setVisible(true);
-            }
-        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
