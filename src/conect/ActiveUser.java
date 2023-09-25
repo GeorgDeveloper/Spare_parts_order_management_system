303977844676a4b74aae40a6bb7f409c7ddc61ad
Georgvit <ifreyd@mail.ru>
2023-09-25 20:56:52 +0300

    add_active_user

---
+++ src/conect/ActiveUser.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conect;
+
+/**
+ *
+ * @author Егор
+ */
+public class ActiveUser {
+    private String activeUserName;
+
+    public String getActiveUserName() {
+        return activeUserName;
+    }
+
+    public void setActiveUserName(String activeUserName) {
+        this.activeUserName = activeUserName;
+    }
+    
+    
+}
